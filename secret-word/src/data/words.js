328f2ce05b6826360b66f6d5c5d73308339db303
Moises Sutil <131698525+moisessutil@users.noreply.github.com>
2023-08-17 22:53:10 -0300

    deploy

---
+++ secret-word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Lanterna", "Carburador", "Limpador-De-Para-Brisa"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Maracuja", "Limão", "Tangerina", "Kiwi"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Orelha", "Ombro", "Panturrilha", "Dedos", "Unha", "Tornozelo"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Fonte", "Controle", "Placa-Mãe", "Headset"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Python", "Angular", "Vue", "Django", "MySQL", "Rust", "Flutter"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Canjica", "Batata", "Torresmo", "Suco", "Queijo", "Farinha", "Trakinas"],
+  };
